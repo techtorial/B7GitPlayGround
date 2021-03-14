aaf98661dcd838af3ba01780b719d28947e6da99
armaganaydingul <76491936+armaganaydingul@users.noreply.github.com>
2021-03-14 12:59:33 -0500

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,10 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Techtorial");
 
+System.out.println("Let's give 10 min break :D ");
+        
+        
+
     }
 
 }
